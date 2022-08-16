945a1eb6de8e42a0843eb7cd87b94deb4b1f09df
pyjcoder <pyjcoder.py@gmail.com>
2022-08-16 21:58:00 +0530

    add more codes

---
+++ easy/programToGet6174/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int find6174Step(int number){
+     // some code here
+     static int step = 0;
+     if (number == 6174)
+     {
+          return step;
+     }
+}
+
+int main(){
+     return 0;
+}
